5be69cea9aa185dfba1cfbaa13e709204da0048b
Yesha Lester <ylester@umich.edu>
2017-08-16 22:35:46 -0400

    Starter Code

--- game.py
+++ game.py
@@ -1,13 +1,23 @@
 # Create a 5 letter word game.
-# Create a list of 5 letter words
+# Create a list of 5 letter words, no word will have letters that appear more than once
 # choose a random word for the game
 # have the user guess 5 letters words
 # Each word will return the number of letters that is in the Winning Word
 # create an option to test the letters chosen by user, if they receive 5/5 they can unscramble and guess the word
 # Only allow ONE guess, if guessed wrong, they fail, else they win the game
 ###########################################################################
+import random
 
+words = ["match", "board", "drink", "guard", "trick", "light", "admit", "doubt"]
+secret_word = random.choice(words)
+print(secret_word)
+letters = []
+for vals in secret_word:
+    letters.append(vals)
+print(letters)
 
-
+guess = input("Guess a 5 letter word:  ")
+for vals in guess:
+    print(vals)
 
 
