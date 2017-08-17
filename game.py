297da0a5b4f91a33541304383b5c9004b1bf3581
Yesha Lester <ylester@umich.edu>
2017-08-16 21:45:08 -0400

    Add gist of game

--- game.py
+++ game.py
@@ -1,4 +1,13 @@
-#Empty python file
-#start here
+# Create a 5 letter word game.
+# Create a list of 5 letter words
+# choose a random word for the game
+# have the user guess 5 letters words
+# Each word will return the number of letters that is in the Winning Word
+# create an option to test the letters chosen by user, if they receive 5/5 they can unscramble and guess the word
+# Only allow ONE guess, if guessed wrong, they fail, else they win the game
+###########################################################################
+
+
+
 
 
